d9b747b9d7adf7636e003a748cdcbe975d00e0fe
Luis Vieira <luisernani87@gmail.com>
2023-04-04 21:08:24 -0300

    2023-04-04

---
+++ app.js
@@ -0,0 +1,31 @@
+function Stopwatch() {
+    let startTime, endTime, running, duration = 0;
+
+    this.start = function () {
+        if(running) throw new error('Stopwatch has already started');
+
+        running = true;
+
+        startTime = new Date();
+    }
+    this.stop = () => {
+        if(!running) throw new error('Stopwatch is not stated');
+
+        running = false
+
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
+    }
+    this.reset = () => {
+        startTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    }
+    Object.defineProperty(this, 'duration', {
+        get: () => {return duration;}
+    });
+}
+
